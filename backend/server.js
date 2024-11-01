3e579f9d39ec7e3f6296d83d7d0f80ac8f4f5e68
Mehdi P <sarri.mehdi@gmail.com>
2024-11-01 03:15:31 +0100

    connecting mongoDb Database

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ console.log("Bi ismi Allah");
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port = process.env.PORT || 8000;
-
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
